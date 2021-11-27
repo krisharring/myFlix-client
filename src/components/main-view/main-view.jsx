3391402d9a46ac485552b7bc43752787881f4c74
Kris <73711936+krisharring@users.noreply.github.com>
2021-11-26 19:01:03 -0500

    Task 3.3

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,34 +1,44 @@
+
+   
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from "../movie-view/movie-view";
 
-class MainView extends React.Component {
+export default class MainView extends React.Component {
 
-    constructor(){
-        super();
-        this.state = {
-            movies: [
-                { _id: 1, Title: 'Movie1', Description: 'TBD', ImagePath: '...'},
-                { _id: 2, Title: 'Movie2', Description: 'TBD', ImagePath: '...'},
-                { _id: 3, Title: 'Movie3', Description: 'TBD', ImagePath: '...'}
-            ]
+  constructor(){
+    super();
+    this.state = {
+      movies: [
+        {_id: 1, Title: "Movie1", Description: "movie description1", ImagePath: 'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_.jpg'},
+        {_id: 2, Title: "Movie2", Description: "movie description2", ImagePath: 'https://m.media-amazon.com/images/M/MV5BZTliNWJhM2YtNDc1MC00YTk1LWE2MGYtZmE4M2Y5ODdlNzQzXkEyXkFqcGdeQXVyMzY0MTE3NzU@._V1_.jpg'},
+        {_id: 3, Title: "Movie3", Description: "movie description3", ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_.jpg'}
+      ],
+      selectedMovie: null
+    };
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie
+    });
+  }
+
+
+  render() {
+    const { movies, selectedMovie } = this.state;
+
+    if (movies.length === 0) return <div className = "main-view">The list is empty!</div>;
+    
+    return (
+      <div className="main-view">
+        {selectedMovie
+          ? <MovieView movie = {selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+          : movies.map(movie => (
+            <MovieCard key = {movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+            ))
         }
-    }
-
-    render() {
-        const movies = this.state.movies;
-        if (movies.length === 0) {
-            return <div className="main-view">
-                The List is Empty
-            </div>
-        } else {
-            return (
-                <div className="main-view">
-                    {movies.map((movie) => {
-                        return <div>{movie.Title}</div>;
-                    })}
-                </div>
-            );
-        }
-    }
+      </div>
+    );
+  }
 }
-
-export default MainView;
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MainView from './components/main-view/main-view';
+import React from "react";
+import ReactDOM from "react-dom";
+import MainView from "./components/main-view/main-view";
 
-// Import statement to indicate that you need to bundle './index.scss'
-import './index.scss';
+//Import statement to indicate that you need to bundle <./index.scss>
+import "./index.scss";
 
-// Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component
-{
-    render() {
-        return (
-            <MainView />
-        );
-    }
+//Main component
+class MyFlixApplication extends React.Component {
+  render() {
+    return (
+      <MainView />
+    );
+  }
 }
 
-// Finds the root of your app
-const container = document.getElementsByClassName('app-container') [0];
+//Finds the root of the app
+const container = document.getElementsByClassName("app-container")[0];
 
-// Tells React to render your app in the root DOM element
+//Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
